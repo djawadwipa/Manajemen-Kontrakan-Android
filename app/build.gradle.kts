d9d1ff53d82eba0897990f944b54e372c00ce768
djawadwipa <fanyagung@gmail.com>
2026-07-31 13:06:04 +0700

    build: wire WorkManager runtime

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,6 +105,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.work.runtime.ktx)
 
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.foundation)
