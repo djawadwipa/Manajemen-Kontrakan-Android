bcf4f4f9af6896f5e9d2d085181b242ba762ca67
djawadwipa <fanyagung@gmail.com>
2026-07-31 14:02:55 +0700

    fix: use lifecycle compose owner

--- app/src/main/java/id/djawadwipa/manajemenkontrakan/ui/ManajemenKontrakanRoot.kt
+++ app/src/main/java/id/djawadwipa/manajemenkontrakan/ui/ManajemenKontrakanRoot.kt
@@ -14,9 +14,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import id.djawadwipa.manajemenkontrakan.ui.screens.AppLockScreen
 import id.djawadwipa.manajemenkontrakan.ui.theme.ManajemenKontrakanTheme
 
